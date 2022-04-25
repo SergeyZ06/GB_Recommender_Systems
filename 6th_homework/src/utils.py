de022927d62b8a4f444aa7fccf51cadcfa205d60
Sergey Zakharchenko <SergeyZ06@yandex.ru>
2022-04-25 21:40:49 +0300

    6th homework - minor changes: - CatBoostClassifier has been set to silent mode; - new function "prepare_result_lvl_2" has been implemented; - level two model features have been renamed "X_train" and "X_test".

--- 6th_homework/src/utils.py
+++ 6th_homework/src/utils.py
@@ -59,19 +59,64 @@ def train_test_split(data: pd.DataFrame,
 
 
 def prepare_result(data: pd.DataFrame,
-                   user_id: str = 'user_id',
-                   item_id: str = 'item_id') -> pd.DataFrame:
+                   feature_user_id: str = 'user_id',
+                   feature_item_id: str = 'item_id',
+                   feature_actual: str = 'actual') -> pd.DataFrame:
     
     '''
     Function for preparing result dataset with users' and items' ids.
     
     data : pd.DataFrame, dataset with history of purchases.
     
-    user_id : str, containts feature name of users' ids.
+    feature_user_id : str, contains feature name of users' ids.
     
-    item_id : str, containts feature name of items' ids.
+    feature_item_id : str, contains feature name of items' ids.
+    
+    feature_actual: str, contains feature name for creating lists of bought items.
     '''
     
-    result = data.copy().groupby('user_id')['item_id'].unique().reset_index()
-    result.columns=['user_id', 'actual']
+    result = data.copy().groupby(feature_user_id)[feature_item_id].unique().reset_index()
+    result.columns=[feature_user_id, feature_actual]
+    return result
+
+
+def prepare_result_lvl_2(data: pd.DataFrame,
+                         feature_user_id: str = 'user_id',
+                         feature_item_id: str = 'item_id',
+                         feature_actual: str = 'actual',
+                         feature_predicted: str = 'predicted') -> pd.DataFrame:
+    
+    '''
+    Function for preparing result dataset with users' and items' ids for second level model.
+    DataFrame will also contain Actual and Predicted flags for training second level model.
+    
+    data : pd.DataFrame, dataset with history of purchases.
+    
+    user_id : str, contains feature name of users' ids.
+    
+    item_id : str, contains feature name of items' ids.
+    
+    feature_actual: str, contains feature name of actual bought items.
+    
+    feature_predicted: str, contains feature name of predicted items by first level model.
+    '''
+    
+    result = data.copy()
+    result = (
+        result
+        .apply(lambda row: pd.Series(row[feature_predicted]), axis=1)
+        .stack()
+        .reset_index(level=1)
+    )
+
+    result[feature_user_id] = result.index
+    result = result.rename(columns={0: feature_item_id})
+    result = result.reset_index(drop=True)
+    result = result.drop(columns=['level_1'])
+    result = result[[feature_user_id, feature_item_id]]
+    result[feature_predicted] = 1
+    result = result.merge(data[[feature_user_id, feature_actual]], on=feature_user_id)
+    result[feature_actual] = result.apply(lambda row: np.isin(row[feature_item_id], row[feature_actual]), axis=1) * 1
+    result[feature_actual] = result[feature_actual].astype(int)
+    
     return result
